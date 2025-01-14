8865429f66f97e6dd214eef3c5a4cde37d45eb02
ilyaigpetrov <ilyaigpetrov@gmail.com>
2025-01-15 02:09:16 +0500

    Add GH actions. Change distDir

---
+++ fleek.config.js
@@ -0,0 +1,10 @@
+/** @type {import('@fleek-platform/cli').FleekConfig} */
+module.exports = {
+  "sites": [
+    {
+      "slug": "decrab",
+      "distDir": "./dist",
+      "buildCommand": ""
+    }
+  ]
+};
